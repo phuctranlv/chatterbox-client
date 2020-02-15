01f3421ff9db22d0189b8ea7e873cbe7b1f72072
phuctranlv <phuctranlv@gmail.com>
2020-02-14 18:26:36 -0800

    fix App.initialize() to display messages (without text)

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,35 +4,42 @@ var App = {
 
   username: 'anonymous',
 
-  initialize: function() {
+  initialize: function () {
     App.username = window.location.search.substr(10);
-
-    FormView.initialize();
-    RoomsView.initialize();
-    MessagesView.initialize();
+    // var Messages = {};
+    // FormView.initialize();
+    // RoomsView.initialize();
+    // MessagesView.initialize();
 
     // Fetch initial batch of messages
     App.startSpinner();
-    App.fetch(App.stopSpinner);
+    App.fetch(App.whenReady);
+
 
   },
 
-  fetch: function(callback = ()=>{}) {
+  fetch: function (callback = () => { }) {
     Parse.readAll((data) => {
-      // examine the response from the server request:
-      console.log(data);
-
+      for (var i = 0; i < data.results.length; i++) {
+        Messages[data.results[i].objectId] = data.results[i];
+      }
       callback();
     });
   },
 
-  startSpinner: function() {
+  startSpinner: function () {
     App.$spinner.show();
     FormView.setStatus(true);
   },
 
-  stopSpinner: function() {
+  stopSpinner: function () {
     App.$spinner.fadeOut('fast');
     FormView.setStatus(false);
+  },
+  whenReady: function () {
+    App.stopSpinner();
+    FormView.initialize();
+    RoomsView.initialize();
+    MessagesView.initialize();
   }
 };
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,22 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    debugger;
+    // var template = _.template('<div><%=username%> said <%=text%> at <%=createdAt%></div>')
+    for (var key in Messages) {
+      var message = Messages[key];
+      var node = $(this.render(message));
+      node.appendTo($('#chats'));
+    }
   },
 
-  render: function() {
-  }
+  render: _.template(`
+
+      <div class="chat">
+        <div class="username"><%=username%></div>
+
+      </div>
+
+    `)
 
 };
